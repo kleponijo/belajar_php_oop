5d5f15143d52dfadbbf3a2ec1403c90804a134b2
kleponijo <posapaaja@gmail.com>
2026-06-04 04:44:21 +0700

    invoke mase

---
+++ 30_Invoke.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "data/14_Student.php";
+
+$student1 = new Student();
+$student1->id = "1";
+$student1->name = "Nadif";
+$student1->value = 100;
+
+$student1(1, "Nadif", true, "Zidan");
